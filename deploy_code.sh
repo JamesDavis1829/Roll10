229c96d258b61f0aa5506e0372706320df6e777c
JamesDavis1829 <jedwarddavis902@gmail.com>
2023-01-18 19:04:06 -0600

    finished build script

--- deploy_code.sh
+++ deploy_code.sh
@@ -3,7 +3,9 @@ dotnet publish -c Release
 cd ../../
 rm -r ./Server/pb_public
 mkdir ./Server/pb_public
-cp ./Client/Roll10/bin/Release/net7.0/wwwroot/_framework ./Server/pb_public/_framework
-cp ./Client/Roll10/bin/Release/net7.0/wwwroot/css ./Server/pb_public/css
-cp ./Client/Roll10/bin/Release/net7.0/wwwroot/images ./Server/pb_public/images
-cp ./Client/Roll10/bin/Release/net7.0/wwwroot/index.html ./Server/pb_public/index.html
+cp -r ./Client/Roll10/bin/Release/net7.0/publish/wwwroot/_framework ./Server/pb_public/_framework
+cp -r ./Client/Roll10/bin/Release/net7.0/publish/wwwroot/css ./Server/pb_public/css
+cp -r ./Client/Roll10/bin/Release/net7.0/publish/wwwroot/images ./Server/pb_public/images
+cp ./Client/Roll10/bin/Release/net7.0/publish/wwwroot/index.html ./Server/pb_public/index.html
+
+./Server/pocketbase serve --http="roll10.org:80" --https="roll10.org:443"
