525dd1b9272a3669ae76a4dbfff0ecb11f39aadf
JamesDavis1829 <jedwarddavis902@gmail.com>
2023-02-03 23:43:33 -0600

    updated script

--- deploy_code.sh
+++ deploy_code.sh
@@ -10,4 +10,4 @@ cp -r ./Client/Roll10/bin/Release/net7.0/publish/wwwroot/js ./Server/pb_public/j
 cp ./Client/Roll10/bin/Release/net7.0/publish/wwwroot/index.html ./Server/pb_public/index.html
 cp ./Client/Roll10/bin/Release/net7.0/publish/wwwroot/redirect.html ./Server/pb_public/redirect.html
 
-./Server/pocketbase serve --http="roll10.org:80" --https="roll10.org:443"
+./Server/pocketbase serve --http="roll10.org:8080" --https="roll10.org:8081"
