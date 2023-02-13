f22d623b59d8921b072fc3d437dbe6892e285c9f
JamesDavis1829 <jedwarddavis902@gmail.com>
2023-02-12 21:29:30 -0600

    fixed toasts staying up until change detection was triggered

--- Client/Roll10Angular/src/app/toast.service.ts
+++ Client/Roll10Angular/src/app/toast.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { delay } from 'lodash';
-import { from, Observable, of, Subscription } from 'rxjs';
+import {from, Observable, of, Subject, Subscription} from 'rxjs';
 
 export type ToastType = "Info" | "Warning";
 export interface IToastMessage {
@@ -15,12 +15,15 @@ export interface IToastMessage {
 export class ToastService {
 
   public toastList: IToastMessage[] = [];
+  public toastListChangeSubject = new Subject<boolean>();
   constructor() { }
 
   public async ShowToast(message: IToastMessage, timeout = 5000)
   {
     this.toastList.push(message);
+    this.toastListChangeSubject.next(true);
     await new Promise<void>(resolve => setTimeout(() => { resolve() }, timeout));
     this.toastList = this.toastList.filter(t => t.id != message.id);
+    this.toastListChangeSubject.next(true);
   }
 }
--- Client/Roll10Angular/src/app/toast/toast.component.ts
+++ Client/Roll10Angular/src/app/toast/toast.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
 import { ToastService } from '../toast.service';
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-toast',
   templateUrl: './toast.component.html',
   styleUrls: ['./toast.component.css']
 })
-export class ToastComponent {
-  constructor(public toastService: ToastService)
+export class ToastComponent implements OnInit, OnDestroy{
+  subs: Array<Subscription> = [];
+  constructor(public toastService: ToastService, private changeRef: ChangeDetectorRef)
   {
-    
+  }
+
+  ngOnDestroy(): void
+  {
+    this.subs.map(s => s.unsubscribe());
+  }
+
+  ngOnInit(): void
+  {
+    this.subs.push(this.toastService.toastListChangeSubject.subscribe(_ => {
+      this.changeRef.detectChanges();
+    }))
   }
 }
