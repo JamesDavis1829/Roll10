82764ba981c74f937ea0b5ff25a869fb8f2a0297
JamesDavis1829 <jedwarddavis902@gmail.com>
2023-03-26 20:02:36 -0500

    Added ancestry pages

--- Client/Roll10Angular/src/app/ancestry-list/ancestry-list.component.ts
+++ Client/Roll10Angular/src/app/ancestry-list/ancestry-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {ICharacter} from "../../domain/data/Character";
+import {PocketBaseService} from "../pocket-base.service";
 
 @Component({
   selector: 'app-ancestry-list',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./ancestry-list.component.css']
 })
 export class AncestryListComponent {
+  public ancestriesList: Array<ICharacter> = [];
 
+  constructor(private pb: PocketBaseService)
+  {
+    this.AsyncOnInit();
+  }
+
+  async AsyncOnInit()
+  {
+    this.ancestriesList = await this.pb.GetFullList<ICharacter>("characters",undefined, "is_ancestor = true");
+  }
 }
